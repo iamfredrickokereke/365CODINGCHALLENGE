62d55c9beb2a541b82e6dcd4599f1c2052cc2865
Kelly Online <kellyonline2016@gmail.com>
2019-02-05 23:28:38 +0100

    initial commit

---
+++ C#/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Namespace
+namespace ConsoleApp1
+{
+    // Main Class
+    class Program
+    {
+        // Entry point method
+        static void Main(string[] args)
+        {
+
+            //START HERE //
+
+            Console.WriteLine("Fredrick, King of the East!");
+
+        }
+    }
+}
