cdfcd08b2c3d97d63099704f95772684eefddf84
Kelly Online <kellyonline2016@gmail.com>
2019-02-24 23:45:59 +0100

    Learning csharp

---
+++ C#/Giraffe/Giraffe/2DArray/2DArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe._2DArray
+{
+    class _2DArray
+    {
+        static void Main()
+        {
+            int[,] matrix =
+            {
+                {1, 2 }, {3, 4}, {5, 6}
+
+            };
+
+            Console.WriteLine(matrix[0, 1]);
+
+            /* string[,] foodTable = new string[3,3];
+
+             foodTable[0, 0] = "egg bread";
+             foodTable[0, 1] = "tea corn";
+             foodTable[1, 0] = "beans garri";
+             foodTable[1, 1] = "akpu egwusi";
+
+             Console.WriteLine(foodTable);
+             */
+
+        }
+    }
+}
